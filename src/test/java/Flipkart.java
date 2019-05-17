4c37d0c3ef8fdd7a68c8cba78d064f6b2cb5fb10
Gaurav Sharma <grvsharma@hotmail.com>
2019-05-17 15:39:06 +0530

    Desired Capabilities

--- src/test/java/Flipkart.java
+++ src/test/java/Flipkart.java
@@ -1,6 +1,12 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class Flipkart {
 
     public static void main(String[] args) {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Test");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "8.0");
 
     }
 }

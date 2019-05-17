422197fbe5f42cb83dca1e68a84a3231f7cfc60d
Gaurav Sharma <grvsharma@hotmail.com>
2019-05-17 15:41:26 +0530

    Mandatory Capabilities

--- src/test/java/Flipkart.java
+++ src/test/java/Flipkart.java
@@ -14,6 +14,11 @@ public class Flipkart {
         capabilities.setCapability("deviceName", "Test");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("appPackage", "com.flipkart.android");
+        capabilities.setCapability("appActivity", "com.flipkart.android.SplashActivity");
+        capabilities.setCapability("app", System.getProperty("user.dir")
+                + "/src/test/resources/app/flipkart.apk");
+
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         WebDriverWait wait = new WebDriverWait(driver, WAIT_TIME);
     }

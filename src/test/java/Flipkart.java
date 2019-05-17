c855f95e78463c45e599176ff353768290f6a3bf
Gaurav Sharma <grvsharma@hotmail.com>
2019-05-17 15:40:33 +0530

    Driver Initialization

--- src/test/java/Flipkart.java
+++ src/test/java/Flipkart.java
@@ -1,12 +1,20 @@
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Flipkart {
+    private static AndroidDriver driver;
+    private static final int WAIT_TIME = 30;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName", "Test");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("platformVersion", "8.0");
-
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        WebDriverWait wait = new WebDriverWait(driver, WAIT_TIME);
     }
 }
